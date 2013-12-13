7897963c7e96679332cee36767f6d6b072e316b7
George Ornbo <george@shapeshed.com>
2013-12-13 14:04:31 +0000

    [feature] add basic routes

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 'use strict';
 
 var Hapi = require('hapi');
-var routes = require('./routes');
+var MongoClient = require('mongodb').MongoClient;
+
 process.env.PORT = process.env.PORT || 8080;
-  //Types = require('hapi').types;
 
+MongoClient.connect('mongodb://pebble:pebble@ds057568.mongolab.com:57568/heroku_app20334957', function (err, db) {
 
-var config = { };
+  if (err)  { throw err; }
 
-var server = new Hapi.Server(+process.env.PORT, '0.0.0.0', config);
+  var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
 
-server.addRoutes(routes);
+  function getHugs(request) {
+    db.collection('hugs', function (err, collection) {
+      collection.find().toArray(function (err, items) {
+        request.reply(items);
+      });
+    });
+  }
 
-server.start();
+  function createHug(request) {
+    var hug = request.payload;
+    db.collection('hugs', function (err, collection) {
+      collection.insert(hug, function (err) {
+        if (err) { throw err; }
+        request.reply(201);
+      });
+    });
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/hugs',
+    config: {
+      handler: getHugs
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/hugs',
+    config: {
+      handler: createHug,
+      payload: 'parse'
+    }
+  });
+
+  server.start();
+
+});
 
